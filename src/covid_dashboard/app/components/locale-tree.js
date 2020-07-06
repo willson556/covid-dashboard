0ebec33c5132a83ffddfa71c4a5e6013f5db29e9
Thomas Willson <thomas.willson@me.com>
2020-07-05 21:49:33 -0700

    Add button to clear locations.

--- src/covid_dashboard/app/components/locale-tree.js
+++ src/covid_dashboard/app/components/locale-tree.js
@@ -1,4 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class LocaleTreeComponent extends Component {
+    @service config;
+
+    @action
+    clear() {
+        this.config.states.clear();
+        this.config.counties.clear();
+    }
 }
