e498b2736cbd11947f93506eb6c4e392622502c1
Thomas Willson <thomas.willson@me.com>
2020-07-06 20:16:40 -0700

    Add front-end display of locale disclaimer.
    Resolves #1.

--- src/covid_dashboard/app/models/county.js
+++ src/covid_dashboard/app/models/county.js
@@ -3,5 +3,6 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class CountyModel extends Model {
     @attr name;
     @attr population;
+    @attr disclaimer;
     @belongsTo('state') state;
 }
