ff01220518999a34993acf012036c7ff591148b4
Thomas Willson <thomas.willson@me.com>
2020-07-12 15:34:00 -0700

    Significantly improve performance by only loading data from cache once per request.
    Also, consolidate methods that are only called from one place.

--- src/covid_api/data_repository.py
+++ src/covid_api/data_repository.py
@@ -52,23 +52,27 @@ def smooth(self):
 pd.Series.smooth = smooth
 del smooth
 
-def get_state_data_path():
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    return str(Path(script_path) / 'static-data/state-population-est2019.csv')
-
-@cache.memoize(timeout=3600)
-def get_state_level_data() -> pd.DataFrame:
-    data = requests.get(TIME_SERIES_STATE_URL).json()
-    for record in data:
-        record["date"] = datetime.strptime(str(record["date"]), "%Y%m%d")
-
-    return pd.DataFrame(data).set_index('date')
-
 @cache.memoize(timeout=86400)
-def get_state_populations() -> pd.DataFrame:
-    state_populations = pd.read_csv(get_state_data_path(), index_col='State', usecols=['State', 'StateFull', '2019'], keep_default_na=False, thousands=',', sep=',', engine='python')
+def get_state_info():
+    counter = 0
+    def transform_state(key, value):
+        nonlocal counter
+        counter += 1
+        return {
+            "id": counter,
+            "name": value["StateFull"],
+            "abbrev": key,
+            "population": value["2019"],
+        }
+
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    csv_path = str(Path(script_path) / 'static-data/state-population-est2019.csv')
+
+    state_populations = pd.read_csv(csv_path, index_col='State', usecols=['State', 'StateFull', '2019'], keep_default_na=False, thousands=',', sep=',', engine='python')
     state_populations["2019"] = state_populations["2019"].astype('int')
-    return state_populations
+
+    state_dict = state_populations.to_dict(orient='index')
+    return [transform_state(key, value) for key, value in state_dict.items()]
 
 @cache.memoize(timeout=86400)
 def get_locale_info() -> pd.DataFrame:
@@ -88,7 +92,15 @@ def get_locale_info() -> pd.DataFrame:
     return locale_data
 
 @cache.memoize(timeout=3600)
-def get_county_level_data() -> pd.DataFrame:
+def get_state_level_data() -> pd.DataFrame:
+    data = requests.get(TIME_SERIES_STATE_URL).json()
+    for record in data:
+        record["date"] = datetime.strptime(str(record["date"]), "%Y%m%d")
+
+    return pd.DataFrame(data).set_index('date')
+
+@cache.memoize(timeout=3600)
+def get_grouped_county_data() -> pd.DataFrame:
     county_case_data = pd.read_csv(io.StringIO(requests.get(TIME_SERIES_US_CASES_COUNTY_URL).text), index_col='date')
     county_case_data.index = pd.to_datetime(county_case_data.index)
 
@@ -96,11 +108,7 @@ def get_county_level_data() -> pd.DataFrame:
         county_case_data.loc[(county_case_data['county'] == fips_merge.county) &
                          (county_case_data['state'] == fips_merge.state), 'fips'] = fips_merge.fake_id
 
-    return county_case_data
-
-@cache.memoize(timeout=600)
-def get_grouped_county_data():
-    return get_county_level_data().groupby('fips')
+    return county_case_data.groupby('fips')
 
 def get_empty_dataframe(start_time: datetime, end_time: datetime) -> pd.DataFrame:
     return pd.DataFrame(index=pd.date_range(start_time, end_time))
@@ -108,6 +116,13 @@ def get_empty_dataframe(start_time: datetime, end_time: datetime) -> pd.DataFram
 class Locale(object):
     population = 0
     name = ""
+
+class RequestCache(object):
+    def __init__(self):
+        self.grouped_county_data = get_grouped_county_data()
+        self.locale_info = get_locale_info()
+        self.state_info = get_state_info()
+        self.grouped_state_data = get_state_level_data().groupby('state')
 
 class Data(object):
     def __init__(self, start_time: datetime, end_time: datetime):
@@ -117,6 +132,7 @@ class Data(object):
         self.positive = get_empty_dataframe(start_time, end_time)
         self.positiveRate = get_empty_dataframe(start_time, end_time)
         self.locales = []
+        self.request_cache = RequestCache()
 
     def _roundtrip(self, obj: pd.DataFrame):
         return json.loads(obj.to_json())
@@ -147,8 +163,8 @@ class DataRepository(object):
         return Data(start_time, end_time)
     
     def add_county_data(self, data: Data, fips):
-        county_data = get_grouped_county_data().get_group(fips)
-        locale_info = get_locale_info().loc[fips]
+        county_data = data.request_cache.grouped_county_data.get_group(fips)
+        locale_info = data.request_cache.locale_info.loc[fips]
 
         locale = Locale()
         locale.population = int(locale_info["Population"])
@@ -159,11 +175,12 @@ class DataRepository(object):
         data.positive[locale.name] = county_data["cases"].diff().smooth()
 
     def add_state_data(self, data: Data, state_id):
-        state_abbrev = self.get_state_data()[state_id - 1]['abbrev']
-        state_data = get_state_level_data().groupby('state').get_group(state_abbrev)
+        state_info = data.request_cache.state_info[state_id - 1]
+        state_abbrev = state_info['abbrev']
+        state_data = data.request_cache.grouped_state_data.get_group(state_abbrev)
 
         locale = Locale()
-        locale.population = int(get_state_populations().loc[state_abbrev, '2019'])
+        locale.population = int(state_info['population'])
         locale.name = state_abbrev
 
         data.locales.append(locale)
@@ -172,27 +189,15 @@ class DataRepository(object):
         data.positive[state_abbrev] = state_data["positiveIncrease"].smooth()
         data.positiveRate[state_abbrev] = ((state_data["positiveIncrease"] * 100) / state_data["totalTestResultsIncrease"]).smooth()
 
-    def get_state_data(self):
-        counter = 0
-        def transform_state(key, value):
-            nonlocal counter
-            counter += 1
-            return {
-                "id": counter,
-                "name": value["StateFull"],
-                "abbrev": key,
-                "population": value["2019"],
-            }
-
-        state_dict = get_state_populations().to_dict(orient='index')
-        return [transform_state(key, value) for key, value in state_dict.items()]
-
     def _get_valid_locales(self):
         locale_info = get_locale_info()
         return locale_info[locale_info.index.notnull()]
 
     def get_available_locales(self):
         return self._get_valid_locales().to_dict(orient='index')
+
+    def get_state_data(self):
+        return get_state_info()
 
     def get_available_locales_for_state(self, state_abbrev):
         def transform_locale(key, value):
